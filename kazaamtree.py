f9534586b843cdaa8698e509284d4943200cf0f9
Jonte <highfestiva@gmail.com>
2016-08-25 13:44:02 +0200

    Optimized tree insertion by using functor instead of if statement.

--- kazaamtree.py
+++ kazaamtree.py
@@ -2,37 +2,43 @@
 
 from coord import coord
 
-bucket_size = 100
-
 class kazaamtree(list):
 	'''A 2D tree structure which quickly finds static objects approximately inside an AABB.
-	   Whenever a bucket is filled, two subnodes are created. On even depths the subnodes
-	   are split in median X, on odd depths they are split on median Y.'''
-	def __init__(self, depth=0):
+	   Whenever a bucket is filled, two subnodes are created. Odd buckets are split by
+	   median X, even buckets are split on median Y.'''
+	def __init__(self, bucket_size=100, depth=0):
+		self.bucket_size = bucket_size
 		self.depth = depth
 		self.splitx = self.splity = None
 		self.lo = self.hi = None
+		self._set_add(self._add)
 
-	def add(self, crd):
-		if self.splitx:
-			(self.lo if crd.x < self.splitx else self.hi).add(crd)
-		elif self.splity:
-			(self.lo if crd.y < self.splity else self.hi).add(crd)
-		else:
-			self.append(crd)
-			l = len(self)
-			if l > bucket_size:
-				if self.depth&1:	# split on x
-					self.sort(key=lambda crd:crd.x)
-					self.splitx = (self[l//2-1].x + self[l//2].x) / 2
-				else: # split on y
-					self.sort(key=lambda crd:crd.y)
-					self.splity = (self[l//2-1].y + self[l//2].y) / 2
-				self.lo = kazaamtree(self.depth+1)
-				self.hi = kazaamtree(self.depth+1)
-				self.lo.extend(self[:l//2])
-				self.hi.extend(self[l//2:])
-				super(kazaamtree, self).clear()
+	def _add(self, crd):
+		self.append(crd)
+		l = len(self)
+		if l > self.bucket_size:
+			if self.depth&1:	# split on x
+				self.sort(key=lambda crd:crd.x)
+				self.splitx = (self[l//2-1].x + self[l//2].x) / 2
+				self._set_add(self._add_split_x)
+			else: # split on y
+				self.sort(key=lambda crd:crd.y)
+				self.splity = (self[l//2-1].y + self[l//2].y) / 2
+				self._set_add(self._add_split_y)
+			self.lo = kazaamtree(self.bucket_size, self.depth+1)
+			self.hi = kazaamtree(self.bucket_size, self.depth+1)
+			self.lo.extend(self[:l//2])
+			self.hi.extend(self[l//2:])
+			super(kazaamtree, self).clear()
+
+	def _add_split_x(self, crd):
+		(self.lo if crd.x < self.splitx else self.hi).add(crd)
+
+	def _add_split_y(self, crd):
+		(self.lo if crd.y < self.splity else self.hi).add(crd)
+
+	def _set_add(self, addfun):
+		self.add = addfun
 
 	def _pr(self):
 		f = self.splitx if self.splitx else (self.splity if self.splity else 0)
@@ -79,27 +85,32 @@ class kazaamtree(list):
 		super(kazaamtree, self).clear()
 		self.splitx = self.splity = None
 		self.lo = self.hi = None
+		self._set_add(self._add)
 
 
 class kazaamindextree(kazaamtree):
-	def __init__(self, depth=0):
-		super(kazaamindextree,self).__init__(depth)
+	def __init__(self, bucket_size=100, depth=0):
+		super(kazaamindextree,self).__init__(bucket_size, depth)
 		self.index = []
 	def add(self, crd):
 		crd.index = len(self.index)
 		self.index.append(crd)
-		super(kazaamindextree,self).add(crd)
+		self._super_add(crd)
 	def clear(self):
 		super(kazaamindextree, self).clear()
 		self.index.clear()
+	def _set_add(self, addfun):
+		self._super_add = addfun
 
 
 if __name__ == '__main__':
 	# Test that fetching via an AABB gets us the approximate number of coords.
 	from random import random
-	tree = kazaamtree()
-	for i in range(1753):
+	tree = kazaamindextree()
+	crdcnt = 17530
+	for i in range(crdcnt):
 		tree.add(coord(random()*20-10, random()*20-10))
-	assert len(tree.get_approx_aabb_crds(coord(-100,-100),coord(100,100))) == 1753
-	assert len(tree.get_approx_aabb_crds(coord(-20,-20),coord(1,+20))) + len(tree.get_approx_aabb_crds(coord(1,-20),coord(20,+20))) <= 1753*1.2
-	assert len(tree.get_approx_aabb_crds(coord(1,-20),coord(20,+20))) >= 1753*9/20*0.8
+	assert len(tree.index) == crdcnt
+	assert len(tree.get_approx_aabb_crds(coord(-100,-100),coord(100,100))) == crdcnt
+	assert len(tree.get_approx_aabb_crds(coord(-20,-20),coord(1,+20))) + len(tree.get_approx_aabb_crds(coord(1,-20),coord(20,+20))) <= crdcnt*1.3
+	assert len(tree.get_approx_aabb_crds(coord(1,-20),coord(20,+20))) >= crdcnt*9/20*0.8
